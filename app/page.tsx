135ea922ed7f3465bda0bb94d4b4643cf85aee81
Muzammil606 <150831372+Muzammil606@users.noreply.github.com>
2023-11-14 08:45:27 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <div style={{ position: 'relative', width: '100vw', height: '200px' }}>
+      <div style={{ position: 'relative', width: '100vw', height: '25vh' }}>
         <Image
           src="https://as2.ftcdn.net/v2/jpg/02/57/54/37/1000_F_257543757_xdx9838dayUXPgLJhIXLVwAdivJX8KEw.jpg"
           alt="Banner"
