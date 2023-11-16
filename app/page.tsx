97edc5013378c17a63936b14cff09d86dd6d0d82
Muzammil606 <150831372+Muzammil606@users.noreply.github.com>
2023-11-16 09:29:37 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="relative flex min-h-screen flex-col items-center">
 
       <div className='relative w-screen h-[20vh]'>
+
         <Image
           src="https://as2.ftcdn.net/v2/jpg/02/57/54/37/1000_F_257543757_xdx9838dayUXPgLJhIXLVwAdivJX8KEw.jpg"
           alt="Banner"
@@ -33,6 +34,124 @@ export default function Home() {
         </div>
 
       </div>
+
+      <div className='relative w-screen h-[80vh]'>
+        <div className='grid grid-flow-col gap-5 px-5 mt-10'>
+          <div className='category'>
+            <div className="category-card mb-5">
+              <div className='relative w-[50px] h-[50px]'>
+                <Image
+                  className='rounded-full'
+                  src="https://as2.ftcdn.net/v2/jpg/02/57/54/37/1000_F_257543757_xdx9838dayUXPgLJhIXLVwAdivJX8KEw.jpg"
+                  alt="Banner"
+                  fill
+                  style={{
+                    objectFit: 'cover'
+                  }}
+                />
+              </div>
+              <h4 className='text-base font-semibold tracking-wide'>Burger</h4>
+            </div>
+            <div className="category-card mb-5">
+              <div className='relative w-[50px] h-[50px]'>
+                <Image
+                  className='rounded-full'
+                  src="https://as2.ftcdn.net/v2/jpg/02/57/54/37/1000_F_257543757_xdx9838dayUXPgLJhIXLVwAdivJX8KEw.jpg"
+                  alt="Banner"
+                  fill
+                  style={{
+                    objectFit: 'cover'
+                  }}
+                />
+              </div>
+              <h4 className='text-base font-semibold tracking-wide'>Burger</h4>
+            </div>
+            <div className="category-card mb-5">
+              <div className='relative w-[50px] h-[50px]'>
+                <Image
+                  className='rounded-full'
+                  src="https://as2.ftcdn.net/v2/jpg/02/57/54/37/1000_F_257543757_xdx9838dayUXPgLJhIXLVwAdivJX8KEw.jpg"
+                  alt="Banner"
+                  fill
+                  style={{
+                    objectFit: 'cover'
+                  }}
+                />
+              </div>
+              <h4 className='text-base font-semibold tracking-wide'>Burger</h4>
+            </div>
+            <div className="category-card mb-5">
+              <div className='relative w-[50px] h-[50px]'>
+                <Image
+                  className='rounded-full'
+                  src="https://as2.ftcdn.net/v2/jpg/02/57/54/37/1000_F_257543757_xdx9838dayUXPgLJhIXLVwAdivJX8KEw.jpg"
+                  alt="Banner"
+                  fill
+                  style={{
+                    objectFit: 'cover'
+                  }}
+                />
+              </div>
+              <h4 className='text-base font-semibold tracking-wide'>Burger</h4>
+            </div>
+            <div className="category-card mb-5">
+              <div className='relative w-[50px] h-[50px]'>
+                <Image
+                  className='rounded-full'
+                  src="https://as2.ftcdn.net/v2/jpg/02/57/54/37/1000_F_257543757_xdx9838dayUXPgLJhIXLVwAdivJX8KEw.jpg"
+                  alt="Banner"
+                  fill
+                  style={{
+                    objectFit: 'cover'
+                  }}
+                />
+              </div>
+              <h4 className='text-base font-semibold tracking-wide'>Burger</h4>
+            </div>
+          </div>
+          <div className="product">
+            <div className="w-fit bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl mb-5">
+              <a href="#">
+                <img src="https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="Product" className="h-30 w-72 object-cover rounded-t-xl" />
+                <div className="px-4 py-3">
+                  <span className="text-gray-400 mr-3 uppercase text-xs">Brand</span>
+                  <p className="text-lg font-bold text-black truncate block capitalize">Product Name</p>
+                  <div className="flex items-center">
+                    <p className="text-lg font-semibold text-black cursor-auto my-3">$149</p>
+                    <del>
+                      <p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+                    </del>
+                    <div className="ml-auto"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-bag-plus" viewBox="0 0 16 16">
+                      <path fill-rule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
+                      <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                    </svg></div>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div className="w-fit bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+              <a href="#">
+                <img src="https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="Product" className="h-30 w-72 object-cover rounded-t-xl" />
+                <div className="px-4 py-3">
+                  <span className="text-gray-400 mr-3 uppercase text-xs">Brand</span>
+                  <p className="text-lg font-bold text-black truncate block capitalize">Product Name</p>
+                  <div className="flex items-center">
+                    <p className="text-lg font-semibold text-black cursor-auto my-3">$149</p>
+                    <del>
+                      <p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+                    </del>
+                    <div className="ml-auto"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-bag-plus" viewBox="0 0 16 16">
+                      <path fill-rule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
+                      <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                    </svg></div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </main>
   )
 }
